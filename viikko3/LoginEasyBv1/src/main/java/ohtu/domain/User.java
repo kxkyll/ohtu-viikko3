2323635a76a2ebbf6e3e330e0786fd1921102a1e
Kati Kyllonen <kati.a.kyllonen@helsinki.fi>
2013-04-06 15:54:44 +0300

    kumpula biershop ja login softat

---
+++ viikko3/LoginEasyBv1/src/main/java/ohtu/domain/User.java
@@ -0,0 +1,29 @@
+package ohtu.domain;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author kxkyllon
+ */
+
+public class User {
+
+    private String username;
+    private String password;
+
+   
+       
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
---
+++ viikko3/LoginEasyBv1/src/main/java/ohtu/services/AuthenticationService.java
@@ -0,0 +1,70 @@
+package ohtu.services;
+
+import ohtu.data_access.UserDao;
+import ohtu.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationService {
+
+    private UserDao userDao;
+
+    @Autowired
+    public AuthenticationService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public boolean logIn(String username, String password) {
+        for (User user : userDao.listAll()) {
+            if (user.getUsername().equals(username)
+                    && user.getPassword().equals(password)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean createUser(String username, String password) {
+        if (userDao.findByName(username) != null) {
+            return false;
+        }
+
+        if (invalid(username, password)) {
+            return false;
+        }
+
+        userDao.add(new User(username, password));
+
+        return true;
+    }
+
+    private boolean invalid(String username, String password) {
+
+        Boolean usernameInvalid = true;
+        Boolean passwordInvalid = true;
+
+        if (username.length() < 3) {
+            return true;
+        }
+        if (password.length() < 8) {
+            return true;
+        }
+
+        if (username.matches("[a-zA-Z]*")) {
+            usernameInvalid = false;
+        }
+
+        if (password.matches(".*\\d.*") || password.matches(".*[!”#$%&’()*+,./;:=?_@>-].*")) {
+            if (password.matches(".*[a-z].*") || password.matches(".*[A-Z].*")) {
+                passwordInvalid = false;
+            }
+
+        }
+        if (!usernameInvalid && !passwordInvalid) {
+            return false;
+        }
+        return true;
+    }
+}
